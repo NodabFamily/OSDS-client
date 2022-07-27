309383913f8d747d4d5da273e9d894dc05633cac
euije <exitfill1234@naver.com>
2022-07-28 08:30:26 +0900

    refactor: Add page routers via react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import styled from "styled-components";
+
+import Home from "./pages/Home";
+import Archive from "./pages/Archive";
+import Bookmark from "./pages/Bookmark";
+import Setting from "./pages/Setting";
+
+import Navbar from "./components/Navbar";
+
+import "./App.css";
+
+const StyledApp = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
 
 function App() {
   return (
     <>
+      <StyledApp>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/archive" element={<Archive />} />
+          <Route path="/bookmark" element={<Bookmark />} />
+          <Route path="/setting" element={<Setting />} />
+        </Routes>
+        <Navbar />
+      </StyledApp>
     </>
   );
 }
