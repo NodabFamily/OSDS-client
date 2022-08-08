16e50a2f0c00ec25416aefe47d261e35781778c1
euije <exitfill1234@naver.com>
2022-08-08 23:02:57 +0900

    refactor: Update hierchy of components

--- src/pages/Bookmark.js
+++ src/pages/Bookmark.js
@@ -1,23 +1,10 @@
 import React from 'react'
 import styled from 'styled-components';
 
-const BookmarkDom = styled.div`
-    width: 100%;
-    height: 94%;
-    background: #FFFFFF;
-
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-`;
-
 const Bookmark = () => {
   return (
     <>
-        <BookmarkDom>
-            <div>Bookmark</div>
-        </BookmarkDom>
+        <div>Bookmark</div>
     </>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,10 @@
 import React from 'react'
 import styled from 'styled-components';
 
-const HomeDom = styled.div`
-    width: 100%;
-    height: 94%;
-    background: #FFFFFF;
-
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-`;
-
 const Home = () => {
   return (
     <>
-        <HomeDom>
-            <div>Home</div>
-        </HomeDom>
+        <div>Home</div>
     </>
   )
 }
