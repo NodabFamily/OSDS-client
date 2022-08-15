d2dd0a44d8446a2702c1b06cb28ce4021a595adc
euije <exitfill1234@naver.com>
2022-08-16 03:45:23 +0900

    feat: Make link from PhotoPage to Modal page

--- src/components/Archive/Comment.js
+++ src/components/Archive/Comment.js
@@ -4,10 +4,10 @@ import styled from "styled-components";
 import { dateToString, STRFTimeToDate } from "../../common/Function/Time";
 
 const UserImg = styled.img`
-    width: 30px;
-    height: 30px;
-    border: 1px solid #E6E6E6;
-    border-radius: 100%;
+  width: 30px;
+  height: 30px;
+  border: 1px solid #e6e6e6;
+  border-radius: 100%;
 `;
 
 const CommentDom = styled.div`
@@ -20,48 +20,73 @@ const CommentDom = styled.div`
   align-items: center;
 `;
 
-const CommentContentDom = styled.div` 
-    background-color: #ECEDF1;
-    border-radius: 5px;
+const CommentContentDom = styled.div`
+  background-color: #ecedf1;
+  border-radius: 5px;
 
-    padding-left: 8px;
-    padding-right: 8px;
-    margin-left: 10px;
-    margin-right: 10px;
+  padding-left: 8px;
+  padding-right: 8px;
+  margin-left: 10px;
+  margin-right: 10px;
 `;
 
 const CommentDate = styled.div`
-    font-family: "Spoqa Han Sans Regular";
-    font-size: 8px;
-    color: #000000;
-    white-space: nowrap;
+  font-family: "Spoqa Han Sans Regular";
+  font-size: 8px;
+  color: #000000;
+  white-space: nowrap;
 
-    margin-top: auto;
-    margin-right: 10px;
+  margin-top: auto;
+  margin-right: 10px;
 `;
 
 const AComment = styled.div`
-    font-family: "Spoqa Han Sans Bold";
-    font-size: 10px;
-    white-space: pre-wrap;
-    
-    margin: 6px 0 6px 0;
+  font-family: "Spoqa Han Sans Bold";
+  font-size: 10px;
+  white-space: pre-wrap;
 
+  margin: 6px 0 6px 0;
 `;
 
 const Comment = (props) => {
-  const commentData = props.comment;
+  const commentData = props.data.comment;
+
+  const onClick = (member) => {
+    props.state.clicked[1]((cur) => (cur = member));
+    props.state.modal[1]((cur) => (cur = true));
+  };
+
+  const getUserInfoById = (userId) => {
+    if (!props.state.family[0].length) {
+      const temp = {
+        id: 0,
+        name: "",
+        nickname: "",
+        bio: "",
+        profile: "",
+      };
+      return temp;
+    }
+    return props.state.family[0].find((user) => user.id === userId);
+  };
 
   return (
     <>
       <CommentDom>
-        <UserImg />
+        <UserImg
+          onClick={() => {
+            onClick(getUserInfoById(commentData.user_id));
+          }}
+          src={getUserInfoById(commentData.user_id).profile}
+        />
         <CommentContentDom>
           <AComment
             style={{
-                fontFamily:"Spoqa Han Sans Regular"
+              fontFamily: "Spoqa Han Sans Regular",
             }}
-            >{commentData.user_id}</AComment>
+          >
+            {getUserInfoById(commentData.user_id).nickname}
+          </AComment>
           <AComment>{commentData.comment}</AComment>
         </CommentContentDom>
         <CommentDate>
